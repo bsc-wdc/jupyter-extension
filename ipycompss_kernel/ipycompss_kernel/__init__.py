daee5f1cc3659e0bd11d69ccc2b026566bc07872
pausmfib <pau.soler.miras@estudiantat.upc.edu>
2023-02-07 11:22:40 +0100

    Create Jupyter kernel that starts/stops PyCOMPSs runtime

---
+++ ipycompss_kernel/ipycompss_kernel/__init__.py
@@ -0,0 +1,1 @@
+from .ipycompss_kernel import IPyCOMPSsKernel
---
+++ ipycompss_kernel/ipycompss_kernel/ipycompss_kernel.py
@@ -0,0 +1,25 @@
+from ipykernel.ipkernel import IPythonKernel
+
+# KernelManager.shutdown_wait_time
+class IPyCOMPSsKernel(IPythonKernel):
+    def start(self):
+        super().start()
+
+        self.shell.run_cell("""
+            import pycompss.interactive as ipycompss
+
+            ipycompss.start(graph=True, monitor=1000) # debug=True, trace=True
+            
+            del ipycompss
+        """, silent=True, store_history=False)
+
+
+
+    def do_shutdown(self, restart: bool):
+        print(self.shell.run_cell("""
+            import pycompss.interactive as ipycompss
+
+            ipycompss.stop(sync=True)
+        """, silent=True, store_history=False))
+        super().do_shutdown(restart)
+
