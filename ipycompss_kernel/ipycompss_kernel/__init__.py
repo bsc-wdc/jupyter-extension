a02c4326418fb113b8ae541d9e3d99c6075c6cd9
pausmfib <pau.soler.miras@estudiantat.upc.edu>
2023-02-07 12:48:50 +0100

    Restructure package

--- ipycompss_kernel/ipycompss_kernel/__init__.py
+++ ipycompss_kernel/ipycompss_kernel/__init__.py
@@ -1,3 +1,4 @@
 """A Jupyter kernel that makes using PyCOMPSs easier"""
 
 from .ipycompss_kernel import IPyCOMPSsKernel
+from .ipycompss_provisioner import IPyCOMPSsProvisioner
--- ipycompss_provisioner/__init__.py
+++
@@ -1,3 +0,0 @@
-"""A provisioner that enables the use of PyCOMPSs Jupyter kernel"""
-
-from .ipycompss_provisioner import IPyCOMPSsProvisioner
