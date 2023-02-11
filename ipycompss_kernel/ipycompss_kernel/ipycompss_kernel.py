8f633028dcf6d95f863a9622a9cc9e460a80eaca
pausmfib <pau.soler.miras@estudiantat.upc.edu>
2023-02-11 19:06:47 +0100

    Divide into functions

--- ipycompss_kernel/ipycompss_kernel/ipycompss_kernel.py
+++ ipycompss_kernel/ipycompss_kernel/ipycompss_kernel.py
@@ -10,21 +10,88 @@ class IPyCOMPSsKernel(IPythonKernel):
         super().start()
 
         self.shell.run_cell('''
-            import tkinter as tk
+            def pycompss_start():
+                import sys
+                import tkinter as tk
 
-            window = tk.Tk()
-            def start():
                 import pycompss.interactive as ipycompss
-                ipycompss.start(graph=True, monitor=1000) # debug=True, trace=True
-                del ipycompss
-                window.destroy()
 
-            window.geometry('250x250')
-            label = tk.Label(window, text='Hello, world!')
-            label.pack(side='top')
-            button = tk.Button(window, text='Start IPyCOMPSs', command=start)
-            button.pack(side='bottom')
-            window.mainloop()
+                def create_window() -> tk.Tk:
+                    window: tk.Tk = tk.Tk()
+                    window.title('IPyCOMPSs configuration')
+                    return window
+
+                def create_title():
+                    nonlocal row
+                    label: tk.Label = tk.Label(window, text='IPyCOMPSs startup options')
+                    label.grid(row=row, column=column, columnspan=2)
+                    row += 1
+
+                def make_boolean_parameter(name: str) -> tk.BooleanVar:
+                    nonlocal row
+                    var: tk.BooleanVar = tk.BooleanVar()
+                    checkbutton: tk.Checkbutton = tk.Checkbutton(
+                        window, text=name.capitalize(), variable=var
+                    )
+                    checkbutton.grid(row=row, column=column, sticky='NSW')
+                    row += 1
+                    return var
+
+                def make_integer_parameter(name: str) -> tk.IntVar:
+                    nonlocal row, column
+                    label: tk.Label = tk.Label(window, text=name.capitalize())
+                    label.grid(row=row, column=column, sticky='NSW')
+                    column += 1
+
+                    var: tk.IntVar = tk.IntVar()
+                    var.set(1000)
+                    spinbox: tk.Spinbox = tk.Spinbox(
+                        window, from_=1, to=sys.maxsize, textvariable=var
+                    )
+                    spinbox.grid(row=row, column=column, sticky='NSW')
+                    row += 1
+                    column = 0
+                    return var
+
+                def create_button():
+                    def start():
+                        arguments: dict = {key: value.get()
+                                           for (key, value) in parameters.items()}
+                        ipycompss.start(**arguments)
+
+                        window.destroy()
+
+                    button: tk.Button = tk.Button(
+                        window, text='Start IPyCOMPSs', command=start
+                    )
+                    button.grid(row=row, column=column, columnspan=2)
+
+                def make_grid_expandable():
+                    for i in range(window.grid_size()[1]):
+                        tk.Grid.rowconfigure(window, i, weight=1)
+                    for i in range(window.grid_size()[0]):
+                        tk.Grid.columnconfigure(window, i, weight=1)
+
+                row: int = 0
+                column: int = 0
+                window: tk.Tk = create_window()
+                create_title()
+
+                boolean_parameters: list = ['graph', 'debug', 'trace']
+                parameters = {
+                    parameter: make_boolean_parameter(parameter)
+                    for parameter in boolean_parameters
+                }
+                parameters['monitor'] = make_integer_parameter('monitor')
+
+                create_button()
+                make_grid_expandable()
+
+                window.mainloop()
+
+
+            pycompss_start()
+            del pycompss_start
         ''', silent=True)
 
     def do_shutdown(self, restart: bool):
