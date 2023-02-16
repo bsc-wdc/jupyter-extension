0607b710ec464223fc9c4d5303f9d104e773708b
pausmfib <pau.soler.miras@estudiantat.upc.edu>
2023-02-16 14:23:40 +0100

    Fix file opening mistake

--- ipycompss_kernel/ipycompss_kernel/parameter/path.py
+++ ipycompss_kernel/ipycompss_kernel/parameter/path.py
@@ -8,11 +8,19 @@ from ipycompss_kernel.parameter.str import StringParameter
 class PathParameter(StringParameter):
     '''Class for path parameters'''
 
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.file: bool = self.default[1]
+        self.default = self.default[0]
+
     def make(self, frame) -> tuple[str, tk.StringVar]:
         var: tk.StringVar = super().make(frame)[1]
 
         def browse():
-            path = filedialog.askopenfilename()
+            if self.file:
+                path = filedialog.askopenfilename()
+            else:
+                path = filedialog.askdirectory()
             var.set(path or '')
 
         button: tk.Button = tk.Button(frame, text='browse', command=browse)
