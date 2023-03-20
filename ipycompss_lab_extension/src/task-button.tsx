b19c113a95d4462cec9fa2b80952150e52d81724
pausmfib <pau.soler.miras@estudiantat.upc.edu>
2023-03-20 16:46:29 +0100

    Improve function detection

--- ipycompss_lab_extension/src/task-button.tsx
+++ ipycompss_lab_extension/src/task-button.tsx
@@ -4,6 +4,13 @@ import { CodeEditor } from '@jupyterlab/codeeditor';
 import React from 'react';
 import { tracker } from './activate';
 
+interface ILineInfo {
+  keyword: string;
+  indentation: number;
+  lineNumber: number;
+  block: boolean;
+}
+
 export const TaskButton = (): JSX.Element => {
   return <ToolbarButtonComponent label="Define task" onClick={createTask} />;
 };
@@ -14,26 +21,67 @@ const createTask = async (): Promise<void> => {
     return;
   }
 
-  const position = editor.getCursorPosition();
-  const offset = editor.getOffsetAt(position);
-  const token: CodeEditor.IToken | undefined = editor
-    .getTokens()
-    .reverse()
-    .find(
-      (token: CodeEditor.IToken): boolean =>
-        token.offset <= offset && token.value === 'def'
-    );
-  if (token === undefined) {
+  const toLineInfo = (token: CodeEditor.IToken): ILineInfo => {
+    const position = editor.getPositionAt(token.offset)!;
+    const line = editor.getLine(position.line)!;
+    const [{}, indent, keyword] = line.match(/^(\s*)(\w+)/)!;
+
+    return {
+      keyword: keyword,
+      indentation: indent.length,
+      lineNumber: position.line,
+      block: /:$/.test(line)
+    };
+  };
+
+  const getCurrentFunctionLineInfo = (): ILineInfo | undefined => {
+    const position = editor.getCursorPosition();
+    const offset = editor.getTokenForPosition(position).offset;
+    const lines: ILineInfo[] = editor
+      .getTokens()
+      .filter(
+        (token: CodeEditor.IToken): boolean =>
+          token.offset === offset ||
+          (token.offset < offset && token.value === ':')
+      )
+      .map(toLineInfo)
+      .filter(
+        (line: ILineInfo, index: number, array: ILineInfo[]) =>
+          index === array.length - 1 || line.block
+      );
+
+    let line = lines.pop();
+    if (line === undefined) {
+      return;
+    } else if (line.keyword === 'def') {
+      return line;
+    }
+
+    let i = lines.length - 1;
+    while (i >= 0) {
+      if (lines[i].indentation < line.indentation) {
+        if (lines[i].keyword === 'def') {
+          return lines[i];
+        } else {
+          line = lines[i];
+        }
+      }
+      --i;
+    }
+    return;
+  };
+
+  const lineInfo = getCurrentFunctionLineInfo();
+  if (lineInfo === undefined) {
     return;
   }
 
-  const tokenPosition = editor.getPositionAt(token.offset);
-  if (tokenPosition === undefined) {
-    return;
-  }
-
-  editor.setCursorPosition(tokenPosition);
+  const linePosition: CodeEditor.IPosition = {
+    column: lineInfo.indentation,
+    line: lineInfo.lineNumber
+  };
+  editor.setCursorPosition(linePosition);
   editor.newIndentedLine();
-  editor.setSelection({ end: tokenPosition, start: tokenPosition });
+  editor.setSelection({ end: linePosition, start: linePosition });
   editor.replaceSelection?.('@task()');
 };
